e5696bc82d27019de588c0425d6d7a66fa8f3ff6
曽我凌太 <sogaryota@sogaryoutanoMacBook-Air.local>
2026-04-10 06:19:51 +0900

    自作バリデーション作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "posts/index"
   get "users/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -14,4 +15,5 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resources :users
+  resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_07_214421) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_08_211945) do
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "title"
     t.date "start"
