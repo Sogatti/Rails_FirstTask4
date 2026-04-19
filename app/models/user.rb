833515fa72818ba3523440090a7d6362845f1e31
曽我凌太 <sogaryota@sogaryoutanoMacBook-Air.local>
2026-04-19 12:44:25 +0900

    バリデーションと国際化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
     validates :title, presence: true, length: { maximum:20 }
     validates :memo, length: { maximum: 500 }
-    validate :start_end_check
+    validates :start, presence: true
+    validates :end, presence: true
+
+    validate :start_end_check,
 
   def start_end_check
-    if self.end < self.start
-    errors.add(:start, "開始日は終了日より前の日付を選択してください")
+    if start.present? && self.end.present?
+      if self.end < self.start
+        errors.add(:start, "は終了日より前の日付を選択してください")
+      end
     end
   end
 end
