404497dbf46fdca48fb0fefe1f182ee484f6b163
曽我凌太 <sogaryota@sogaryoutanoMacBook-Air.local>
2026-04-26 15:47:04 +0900

    post関連の追加削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_08_211945) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_26_063742) do
   create_table "users", force: :cascade do |t|
     t.string "title"
     t.date "start"
